cfa6f011d98eba682fa04a34ca98e520c7f16d03
AleRojas27 <alejandro.rrc99@hotmail.com>
2020-06-09 03:49:37 -0500

    Termine modulo Mercado Libre

---
+++ vista/modulos/prueba.php
@@ -0,0 +1,91 @@
+<div class="container estiloContainer shadow-sm">
+
+    <div class="row">
+
+        <div class="col-3 estiloHijo">
+
+            <div class="row">
+            
+                <div class="col-4">
+                    <img src="https://http2.mlstatic.com/resources/frontend/homes-korriban/assets/images/payments/credit-card.svg" alt="" class="">
+                </div>
+
+                <div class="col-8">
+                    <h5>
+                        Paga con hasta 12 MSI
+                    </h5>
+                    <a href="https://google.com.mx">
+                        Ver más
+                    </a>
+                </div>
+            
+            </div>
+        
+        </div>
+
+        <div class="col-3 estiloHijo lineaDerecha">
+        
+            <div class="row">
+            
+                <div class="col-4">
+                    <img src="https://http2.mlstatic.com/resources/frontend/homes-korriban/assets/images/payments/debit-card.svg" alt="">
+                </div>
+
+                <div class="col-8">
+                    <h5>
+                        Tarjeta de Debito
+                    </h5>
+                    <a href="https://google.com.mx" class="verMas">
+                        <p>Ver más</p>
+                    </a>
+                </div>
+            
+            </div>
+        
+        </div>
+
+        <div class="col-3 estiloHijo">
+        
+            <div class="row">
+            
+            <div class="col-4">
+                <img src="https://http2.mlstatic.com/resources/frontend/homes-korriban/assets/images/payments/view-more.svg" alt="">
+            </div>
+
+            <div class="col-8">
+                <h5>
+                    Mas medios de pago
+                </h5>
+                <a href="https://google.com.mx">
+                    Ver mas
+                </a>
+            </div>
+            
+            </div>
+        
+        </div>
+        
+        <div class="col-3 estiloHijo lineaDerecha">
+        
+            <div class="row">
+            
+                <div class="col-4">
+                    <img src="https://http2.mlstatic.com/resources/frontend/homes-korriban/assets/images/payments/payment-agreement.svg" alt="">
+                </div>
+
+                <div class="col-8">
+                    <h5>
+                        Efectivo, deposito y transferencia
+                    </h5>
+                    <a href="https://google.com.mx" class="verMas">
+                        <p>Ver mas</p>
+                    </a>
+                </div>
+            
+            </div>
+
+        </div>
+    
+    </div>
+
+</div>
